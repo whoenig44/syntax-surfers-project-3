25a45d9ceb4cbae2fe5d627e2c46fa0bbc285b30
AmySuperCoder <tribegriffith@gmail.com>
2025-02-25 20:30:01 -0500

    changed some of the css

--- client/src/Components/ui/button.tsx
+++ client/src/Components/ui/button.tsx
@@ -7,7 +7,7 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
 export const Button: React.FC<ButtonProps> = ({ className, children, ...props }) => {
   return (
     <button
-      className={`bg-blue-500 text-white rounded-md p-2 hover:bg-blue-600 transition-all ${className}`}
+      
       {...props}
     >
       {children}
