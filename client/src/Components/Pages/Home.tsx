e2e2a2bb7e3adc5da53d25db8b52ebacbd03a78b
AmySuperCoder <tribegriffith@gmail.com>
2025-02-26 14:27:23 -0500

    Made home page beautiful

--- client/src/Components/Pages/Home.tsx
+++ client/src/Components/Pages/Home.tsx
@@ -1,5 +1,6 @@
 
 import SearchForm from '../SearchForm';
+import logo from '../../assets/LibraryBackdrop.jpg';
 
 
 
@@ -7,10 +8,13 @@ import SearchForm from '../SearchForm';
 export default function Home() {
     return (
        <>
-      
+      <h2>Welcome to The Library!</h2>
+      <p>Search for a book by author's name, title, or category.</p>
+      <p>When you find a book you like, you can choose to check it out.</p>
+      <p>Click the My Books tab to see which books you currently have checked out.</p>
        
         <SearchForm/>
-           
+        <img src={logo} alt="Library image" style={{height: "375px"}} />           
             
             
         
