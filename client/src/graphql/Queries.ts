9fbbba38124894bdb03e5b173f191063790a05b2
ShannonJTaylor <shannonjo8475@gmail.com>
2025-03-05 18:44:48 -0500

    Commented out the graphql code as it isn't being called anywhere yet.

--- client/src/graphql/Queries.ts
+++ client/src/graphql/Queries.ts
@@ -1,69 +1,69 @@
-import { gql, useQuery } from '@apollo/client';
+// import { gql, useQuery } from '@apollo/client';
 
-// Query to get books by category
-export const GET_BOOKS_BY_CATEGORY = gql`
-  query getBooksByCategory($category: String!) {
-    booksByCategory(category: $category) {
-      id
-      title
-      author
-      publicationDate
-      categories
-    }
-  }
-`;
+// // Query to get books by category
+// export const GET_BOOKS_BY_CATEGORY = gql`
+//   query getBooksByCategory($category: String!) {
+//     booksByCategory(category: $category) {
+//       id
+//       title
+//       author
+//       publicationDate
+//       categories
+//     }
+//   }
+// `;
 
-// Custom hook to use getBooksByCategory query
-export const useGetBooksByCategory = (category: string) => {
-  const { loading, error, data } = useQuery(GET_BOOKS_BY_CATEGORY, {
-    variables: { category },
-  });
+// // Custom hook to use getBooksByCategory query
+// export const useGetBooksByCategory = (category: string) => {
+//   const { loading, error, data } = useQuery(GET_BOOKS_BY_CATEGORY, {
+//     variables: { category },
+//   });
 
-  return {
-    loading,
-    error,
-    books: data ? data.booksByCategory : [],
-  };
-};
+//   return {
+//     loading,
+//     error,
+//     books: data ? data.booksByCategory : [],
+//   };
+// };
 
-// Query to get books by search parameters (title, author, category)
-export const GET_BOOKS_BY_SEARCH = gql`
-  query getBooksBySearch($title: String, $authorFirstName: String, $authorLastName: String, $category: String) {
-    getBooksBySearch(
-      title: $title,
-      authorFirstName: $authorFirstName,
-      authorLastName: $authorLastName,
-      category: $category
-    ) {
-      id
-      title
-      author
-      categories
-    }
-  }
-`;
+// // Query to get books by search parameters (title, author, category)
+// export const GET_BOOKS_BY_SEARCH = gql`
+//   query getBooksBySearch($title: String, $authorFirstName: String, $authorLastName: String, $category: String) {
+//     getBooksBySearch(
+//       title: $title,
+//       authorFirstName: $authorFirstName,
+//       authorLastName: $authorLastName,
+//       category: $category
+//     ) {
+//       id
+//       title
+//       author
+//       categories
+//     }
+//   }
+// `;
 
-// Custom hook to use getBooksBySearch query
-export const useGetBooksBySearch = (title: string, authorFirstName: string, authorLastName: string, category: string) => {
-  const { loading, error, data } = useQuery(GET_BOOKS_BY_SEARCH, {
-    variables: { title, authorFirstName, authorLastName, category },
-  });
+// // Custom hook to use getBooksBySearch query
+// export const useGetBooksBySearch = (title: string, authorFirstName: string, authorLastName: string, category: string) => {
+//   const { loading, error, data } = useQuery(GET_BOOKS_BY_SEARCH, {
+//     variables: { title, authorFirstName, authorLastName, category },
+//   });
 
-  return {
-    loading,
-    error,
-    books: data ? data.getBooksBySearch : [],
-  };
-};
+//   return {
+//     loading,
+//     error,
+//     books: data ? data.getBooksBySearch : [],
+//   };
+// };
 
-export const GET_USER_BOOKS = gql`
-  query getUserBooks($userId: ID!) {
-    userBooks(userId: $userId) {
-      _id
-      bookId
-      checkedOut
-      checkoutDate
-      returnDate
-    }
-  }
-`;
+// export const GET_USER_BOOKS = gql`
+//   query getUserBooks($userId: ID!) {
+//     userBooks(userId: $userId) {
+//       _id
+//       bookId
+//       checkedOut
+//       checkoutDate
+//       returnDate
+//     }
+//   }
+// `;
