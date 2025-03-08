1716d0f564955bd7319b9a6a27457a471cfaabc6
AmySuperCoder <tribegriffith@gmail.com>
2025-03-08 09:17:07 -0500

    added About page to edit later

--- client/src/Components/Pages/About.tsx
+++ client/src/Components/Pages/About.tsx
@@ -1,24 +1,43 @@
-import React from 'react';
+// import "./About.css";
+
 
 export default function About() {
     return (
       <div>
         <h1>About</h1>
         <p>
-          Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula.
-          Curabitur pellentesque turpis sit amet eros iaculis, a mollis arcu
-          dictum. Ut vel ante eget massa ornare placerat. Etiam nisl orci, finibus
-          sodales volutpat et, hendrerit ut dolor. Suspendisse porta dictum nunc,
-          sed pretium risus rutrum eget. Nam consequat, ligula in faucibus
-          vestibulum, nisi justo laoreet risus, luctus luctus mi lacus sit amet
-          libero. Class aptent taciti sociosqu ad litora torquent per conubia
-          nostra, per inceptos himenaeos. Mauris pretium condimentum tellus eget
-          lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-          Donec placerat accumsan mi, ut congue neque placerat eu. Donec nec ipsum
-          in velit pellentesque vehicula sit amet at augue. Maecenas aliquam
-          bibendum congue. Pellentesque semper, lectus non ullamcorper iaculis,
-          est ligula suscipit velit, sed bibendum turpis dui in sapien.
+        Syntax Surfers is a group dedicated to bringing you high-quality apps that make it easier for you to manage the day-to-day logistics
+        of your life. We create applications that are user-friendly and intuitive and ultimately save you time so that you 
+        have more time to focus on what's important to you.
         </p>
+        <h3>Health Is Wealth!</h3>
+        <p>
+          Our latest app makes it easier for you to track whether certain behaviors are affecting your health. Because every 
+          body is different, you choose the behaviors that you feel may be affecting you. This could be eating certain ingredients, 
+          like gluten or dairy, or it could be other behaviors like exercising or getting at least 8 hours of sleep.
+        </p>
+        <p>
+          Once you decide what behaviors you want to track, Health Is Wealth! provides a simple interface so that you can record whether 
+          you engaged in that behavior each day. You also log how you feel each day, so that you can see if there is a correlation between 
+          the behaviors you're tracking and how you feel.
+        </p>
+        <h3>Other Apps</h3>
+        <p>
+          Check out our first app--<a href="https://whoenig44.github.io/bootcamp-project-1/">GLO (Grocery List Organizer)</a>. This app 
+          allows you to create lists of the items you normally buy at various stores. Before you make a trip to that store, you can 
+          check off items that you're running low on. You can also also add items that you need to buy this time that aren't on 
+          your list of regular purchases
+        </p>
+
+      <h3>Our Team</h3>
+      <p>
+        Our team is made up of four developers with a wide variety of backgrounds and skills who are passionate about creating 
+        apps that make your life easier. Shelia, Amy, Will, and Shannon have had a variety of job titles, including scientist, 
+        engineer, business owner, English teacher, Realtor, kick boxing instructor, stand up paddleboard yoga instructor, butterfly wrangler, and more. 
+        We bring all of our unique experiences to the table to create apps that are useful and intuitive for a customer base that's 
+        as diverse as we are.
+       
+      </p>
       </div>
     );
   }
