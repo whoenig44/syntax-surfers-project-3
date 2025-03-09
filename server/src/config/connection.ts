496b8c540f3c8071f44b5d6951952f85db0df66c
whoenig44 <will.hoenig44@gmail.com>
2025-03-09 15:16:56 -0500

    Editing connection to Mongo code

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -3,9 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config(); // Load environment variables
 
-const activeUser = process.env.ACTIVE_USER || "USER1";
-
-const MONGODB_URI = process.env[`MONGODB_URI_${activeUser.toUpperCase()}`] || "mongodb://127.0.0.1:27017/LibraryApp";
+const MONGODB_URI = process.env[`MONGODB_URI`] || "mongodb://127.0.0.1:27017/LibraryApp";
 console.log(MONGODB_URI, "Mongo connection URI");    
 
 //testing my branch
