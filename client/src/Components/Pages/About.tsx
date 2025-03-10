b150030182bb4b872f57436471cbd185e0cd33bd
AmySuperCoder <tribegriffith@gmail.com>
2025-03-09 22:00:39 -0400

    updated the About page

--- client/src/Components/Pages/About.tsx
+++ client/src/Components/Pages/About.tsx
@@ -10,16 +10,10 @@ export default function About() {
         of your life. We create applications that are user-friendly and intuitive and ultimately save you time so that you 
         have more time to focus on what's important to you.
         </p>
-        <h3>Health Is Wealth!</h3>
+        <h3>The Library</h3>
         <p>
-          Our latest app makes it easier for you to track whether certain behaviors are affecting your health. Because every 
-          body is different, you choose the behaviors that you feel may be affecting you. This could be eating certain ingredients, 
-          like gluten or dairy, or it could be other behaviors like exercising or getting at least 8 hours of sleep.
-        </p>
-        <p>
-          Once you decide what behaviors you want to track, Health Is Wealth! provides a simple interface so that you can record whether 
-          you engaged in that behavior each day. You also log how you feel each day, so that you can see if there is a correlation between 
-          the behaviors you're tracking and how you feel.
+        Our latest app is the ultimate resource for people wanting to improve their coding and IT skills. With the help of an app, you have access to the ultimate library of 
+        technology-related books. Check these books out, and you're on the fast-track to becoming a coding master.
         </p>
         <h3>Other Apps</h3>
         <p>
@@ -28,13 +22,17 @@ export default function About() {
           check off items that you're running low on. You can also also add items that you need to buy this time that aren't on 
           your list of regular purchases
         </p>
+        <p>
+          Another app, <a href="https://github.com/whoenig44/syntax-surfers-project-2">Health Is Wealth!</a>, is a great resource for users who want to track how certain behaviors or foods affect their health. Users determine which behaviors
+          they want to track, and then they record how they are feeling each day. A number of graphs allow users to see how these things are related. You are what you eat, and Health Is 
+          Wealth! helps you see how what you eat affects how you feel.
+        </p>
 
       <h3>Our Team</h3>
       <p>
-        Our team is made up of four developers with a wide variety of backgrounds and skills who are passionate about creating 
-        apps that make your life easier. Shelia, Amy, Will, and Shannon have had a variety of job titles, including scientist, 
-        engineer, business owner, English teacher, Realtor, kick boxing instructor, stand up paddleboard yoga instructor, butterfly wrangler, and more. 
-        We bring all of our unique experiences to the table to create apps that are useful and intuitive for a customer base that's 
+        Our team is made up of developers with a wide variety of backgrounds and skills who are passionate about creating 
+        apps that make your life easier. Will, Shannon, and Amy work as an engineer, scientist, and engineer by day and by night toil under the direction of Stanley to bring you the latest 
+        in great apps. We bring all of our unique experiences to the table to create apps that are useful and intuitive for a customer base that's 
         as diverse as we are.
        
       </p>
